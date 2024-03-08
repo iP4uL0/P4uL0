b2d408240a0063455c68b875227b77f228235b1a
iP4uL0 <160270216+iP4uL0@users.noreply.github.com>
2024-03-08 14:51:31 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,11 @@
+const entrada = require('readline-sync')
+
+//  let numero =  parseFloat = (entrada.question('digite um numero:'))
+//  let raiz = Math.sqrt(numero)
+// console.log(raiz)
+if (10>8){
+console.log('ola')
+}
+else{
+console.log('tchau')
+}
---
+++ aula4.js
@@ -0,0 +1,42 @@
+//exercicio 01
+ const entrada = require ('readline-sync')
+// let horas = parseFloat(entrada.question('Digite as horas trabalhadas no mes: '))
+// let salario_por_hora = parseFloat(entrada.question('Digite seu salario por hora: '))
+// let salario_final = (horas * salario_por_hora)
+// let salario_final2 = ((horas - 160) * (salario_final * 1.5) + salario_final) 
+// if(horas>160){
+//     console.log(salario_final2)
+// }
+// else{
+//     console.log(salario_final)
+// }
+// exercicio 2
+// let salario = parseFloat(entrada.question('Digite seu salario: '))
+// let vendas = parseFloat(entrada.question('Digite suas vendas: '))
+// let valorvenda = (vendas * 0.03) + salario
+
+// if(valorvenda<= 1500){
+//     console.log(valorvenda)
+// }
+// else{
+//     let comissao = 0.05 * (valorvenda - 1500)
+//     let comissao2 = 1500 *0.03
+//     let sarlariofixo = salario + comissao + comissao2
+//     console.log(salario)
+// }
+// exercicio 3
+// let saldo = parseFloat(entrada.question('digite seu saldo: '))
+// let credito = parseFloat(entrada.question('digite seu credito: '))
+// let debito = parseFloat(entrada.question('digite seu debito: '))
+// let saldoautal = (saldo - debito + credito)
+// if(saldoautal>=0){
+//     console.log('saldo positivo')
+// }
+// else{
+//     console.log('saldo negativo')
+// }
+
+let preco =  parseFloat(entrada.question('digite o preco do litro: '))
+let bomba = parseFloat(entrada.question('digite os km: '))
+
+console.log(bomba)
