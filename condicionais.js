ea5e3adf8db3296e84c9c3da03d02ff536eb8426
iP4uL0 <160270216+iP4uL0@users.noreply.github.com>
2024-03-08 14:50:08 -0300

    Add files via upload

---
+++ condicionais.js
@@ -0,0 +1,37 @@
+const entrada = require('readline-sync')
+
+// let idade = parseInt(entrada.question('Qual a sua idade?:'))
+// if(idade>=18){
+//     console.log('maior de idade')
+// }
+// else{
+//     console.log(' menor de idade')
+// }
+// let numero = parseFloat(entrada.question('Digite um numero:'))
+
+// if (numero != 2 === 0){
+//     console.log('Par')
+// }
+// else{
+//     console.log('impar')
+// }
+// let nota1 = parseFloat(entrada.question('Digite sua nota:'))
+// let nota2 = parseFloat(entrada.question('Digite sua nota:'))
+// let nota3 = parseFloat(entrada.question('Digite sua nota:'))
+// let nota4 = parseFloat(entrada.question('Digite sua nota:'))
+// let nota_final = (nota1 + nota2 + nota3 + nota4) / 4
+// if (nota_final >=7){
+//     console.log('Aprovado')
+// }
+// else{
+//     console.log('Reprovado')
+// }
+// let num1 = parseFloat(entrada.question('Digite seu numero:'))
+// let num2 = parseFloat(entrada.question('Digite seu numero:'))
+// if (num1>num2){
+//     console.log(num1)
+// }
+// else{
+// console.log(num2)
+// }
+    
---
+++ execicios_bruno.js
@@ -0,0 +1,40 @@
+const entrada = require ('readline-sync')
+// exer001
+//  let valor = (entrada.question('Digite um valor:'))
+//  if(valor>=1){
+//   console.log('positivo')
+// }
+//  if(valor<= -1){
+//      console.log(negativo)
+//  }
+//  if(valor==0){
+//      console.log('zero')
+//  }
+//exer002
+// let valor1 = parseFloat(entrada.question('digite um valor:'))
+// let valor2 = parseFloat(entrada.question('digite um valor:'))
+// let valor3 = parseFloat(entrada.question('digite um valor:'))
+// let valor_final = [valor1,valor2,valor3]
+// valor_final.sort()
+// console.log(valor_final)
+// exe003
+// let time = (entrada.question('qual o seu time:'))
+// let time2 = (entrada.question('qual o seu time:'))
+// let valor_time = parseFloat(entrada.question('saldo de gols: '))
+// let valor_time2 = parseFloat(entrada.question('saldo de gols: '))
+// if(valor_time>valor_time2){
+//     console.log('o time '+time +' ganhou a partida')
+// }
+// if(valor_time<valor_time2){
+//     console.log('o time '+time2 +' ganhou a partida')
+// }
+// if(valor_time==valor_time2){
+//     console.log('O jogo empatou')
+// }
+// exer004
+// let idade_homem = parseFloat(entrada.question('digite a sua idade: '))
+// let idade_homem2 = parseFloat(entrada.question('digite a sua idade: '))
+// let idade_mulher = parseFloat(entrada.question('digite a sua idade: '))
+// let idade_mulher2 = parseFloat(entrada.question('digite a sua idade: '))
+
+// if()
