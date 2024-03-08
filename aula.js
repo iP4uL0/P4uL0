be95cc3ffec2793c34826801a1c89a9b71299129
iP4uL0 <160270216+iP4uL0@users.noreply.github.com>
2024-03-08 14:52:56 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,29 @@
+//Criando e manipulando variaveis
+/*
+// serve para fazer comentários no meio do código
+*/
+let numero = 10
+let numero_dois = 20
+numero_dois = numero
+console.log(numero_dois)
+console.log(numero)
+
+let nome = 'Paulo'
+let sobrenome = 'Santos'
+//concatenção para juntar textos para ser uma coisa só
+let nome_completo = nome +" "+sobrenome
+//nome+espaço+sobrenome para juntar textos 
+console.log(nome_completo)
+//concatenando strings com +
+//template strings
+nome_completo = ` Bem Vindo de Volta ${nome} ${sobrenome}`
+console.log(nome_completo)
+//concateando com ` texto ${variavel}`
+let valor = 3
+let valor2 = 5
+valor = valor + 30
+console.log(valor2)
+let num = 2
+let num_dois = 3
+let soma = num * 20
+console.log(soma)
---
+++ aula2.js
@@ -0,0 +1,32 @@
+//Segunda aula
+ const entrada = require('readline-sync')
+// let nome = entrada.question('Qual o seu nome?:')
+//.question para entrada de dados 
+// console.log("Bem vindo " + nome)
+ let idade = entrada.question("Qual sua idade?:")
+// console.log("Muito legal e Bem vindo "+ nome + " " +"com "  + idade + " " +"anos")
+ let num = parseFloat (entrada.question("Digite um numero:"))
+ let num42 = parseFloat (entrada.question("Digite outro numero:"))
+  let multiplicacao = num + num2
+//  console.log(multiplicacao)
+let num1 = parseFloat (entrada.question("digite um numero:"))
+ let num2 = parseFloat (entrada.question("digite um numero:"))
+ let num3 = parseFloat (entrada.question("digite um numero:"))
+ let num4 = parseFloat(entrada.question("digite um numero:"))
+ let media = (num1 + num2 + num3 + num4) /4
+// console.log(media)
+//F = C x 1,8 + 32
+ let celcius = parseFloat(entrada.question('temperatura em celcius:'))
+ let temperaturaF = celcius * 1.8 + 32
+// console.log("A temperatura F é: " + temperaturaF)
+//toUpperCase() e toLowerCase()
+ let frase7 = "HELLO WORLD"
+  let frase_maicusculo=frase.toUpperCase()
+  let frase_minusculo = frase.toLowerCase()
+//console.log(frase_minusculo)
+let senha = " adm "
+let senha_corrigida = senha.trim()
+// console.log(senha_corrigida)
+let frase = " hoje fui a escola"
+let frase2 = frase.toUpperCase().trim()
+console.log(frase2)
